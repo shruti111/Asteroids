4d6acc54e230312e013d2479976a8f2b4a6e60bd
shruti111 <shrutichoksi111@gmail.com>
2016-08-03 18:43:15 +0530

    Closures in Swif

---
+++ Closures.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let studname = { print("Welcome to Swift Closures") }
+studname()
+
+
+let divide = {
+    (val1: Int, val2: Int) -> Int in
+    return val1 / val2
+}
+
+let result = divide(200, 20)
+print (result)
+
+func backwards(s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
